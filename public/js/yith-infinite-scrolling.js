734ddacf8de8353a9d7c05fda1764fc33656167d
PaulWijnberg <contact@wijnberg.dev>
2024-02-27 10:58:12 +0100

    Wait 100 milliseconds for Yith scroll event

--- public/js/yith-infinite-scrolling.js
+++ public/js/yith-infinite-scrolling.js
@@ -1,6 +1,8 @@
 ( function ( $, window, document ) {
 	'use strict';
 	$( document ).on( 'yith-infs-scroll-finished', function () {
-		woo_swiper_init_swiper();
+		setTimeout(function(){
+			woo_swiper_init_swiper();
+		},100)
 	} );
 } )( jQuery, window, document );
